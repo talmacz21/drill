99368222bb8b8f4d24510da23a2e1f6a69b59254
talmacz <cvmacua@up.edu.ph>
2023-11-13 12:29:55 +0800

    transpose

--- table2.py
+++ table2.py
@@ -1,10 +1,25 @@
-import pandas as pd
+import pandas as pd 
+import random 
 # Load the CSV data into a DataFrame
 df = pd.read_csv('table1.csv').dropna()
 # Create a new column with calculated values
 df['id'] = range(1,61)
 # Write the updated DataFrame back to a CSV file
-df.to_csv('table2.csv', index=False)
-#add a new column  with prefix meta
-df['date'] = 'META-'+df['date'].astype(str)
 
+#add a new column  with prefix meta
+
+df['Date']='META-'+ df['Date']
+print(df)
+#add a new column with randomly assigned 0,1
+
+dummy = []
+for c in range (0,60):
+    r = random.randint(0,1)
+    dummy.append(r)
+df['random'] =dummy 
+print(df)
+# Transpose the rows as columns
+transposed_df = df.transpose()
+print(transposed_df)
+df.to_csv('table2.csv', index=False)
+transposed_df.to_csv('table3.csv', index=False)
